7ea1e7eadcbb75fd82aa86b141bf4c9e81eed201
SurovtsevaVictoriia <78654989+SurovtsevaVictoriia@users.noreply.github.com>
2021-04-20 18:52:57 +0300

    base

---
+++ remote_server.cpp
@@ -0,0 +1,116 @@
+#include <iostream>
+#include <string>
+#include <boost/asio.hpp>
+
+void print(std::string text) {
+	std::cout << "-------------------------------\n" <<
+		"message from client:\n" <<
+		text <<
+		"\n-------------------------------\n";
+}
+
+void long_write_data(boost::asio::ip::tcp::socket& socket) {
+	std::cout << "write_thread started\n";
+	
+	std::string text;
+	boost::system::error_code error;
+	while (std::getline(std::cin, text) && (text != "cancel")) {
+		text += '\n';
+		boost::asio::write(socket, boost::asio::buffer(text), error);
+
+		if (!error) {
+			std::cout << "Server sent message:\t" << text << std::endl;
+		}
+		else {
+			std::cout << "send failed: " << error.message() << std::endl;
+			return;
+		}
+	}
+}
+
+void long_receive_data(boost::asio::ip::tcp::socket& socket) {
+
+
+	std::cout << "read_thread started\n";
+
+	boost::asio::streambuf buffer;
+	boost::system::error_code error;
+
+	while (true) {
+
+		boost::asio::read_until(socket, buffer, '\n', error);
+
+		if (!error) {
+			std::string message;
+			std::istream input_stream(&buffer);
+			std::getline(input_stream, message, '\n');
+			print(message);
+		}
+		else {
+			std::cout << "send failed: " << error.message() << std::endl;
+			return;
+		}
+	}
+	return;
+}
+
+
+int main(int argc, char** argv)
+{
+	system("chcp 1251");
+
+	using namespace boost::asio;
+	//--------------------------------------------------------------------------
+	auto port = 3333;
+	
+	
+	std::string raw_ip_address = "127.0.0.1";
+	
+	boost::system::error_code error_code;
+	ip::address ip_address =ip::address::from_string(raw_ip_address, error_code);
+	ip::tcp::endpoint endpoint_1(ip_address, port);
+	ip::tcp::endpoint endpoint_2(ip_address, 9999);
+	std::cout << "Endpoint ready" << std::endl;
+
+	io_service io_service_;
+	ip::tcp protocol_ = boost::asio::ip::tcp::v4();
+	//--------------------------------------------------------------------------
+	try
+	{	
+		//--------------------------------------------------------------------------
+		ip::tcp::acceptor receive_acceptor(io_service_, endpoint_1);
+		ip::tcp::socket recieve_socket(io_service_);
+		receive_acceptor.accept(recieve_socket);
+		std::cout << "receive soccet accepted\n";
+
+		ip::tcp::acceptor send_acceptor(io_service_, endpoint_2);
+		ip::tcp::socket send_socket(io_service_);
+		std::cout << "waiting...\n";
+		send_acceptor.accept(send_socket);
+
+		std::cout << " send soccet accepted\n";
+		//--------------------------------------------------------------------------
+
+		std::thread read_tread(long_receive_data, std::ref(recieve_socket)/*, std::ref(io_service_), std::ref(endpoint_)*/);
+		std::thread write_thread(long_write_data, std::ref(send_socket)/*, std::ref(io_service_), std::ref(endpoint_)*/);
+
+		read_tread.join();
+		write_thread.join();
+
+		
+
+	}
+	catch (boost::system::system_error& e)
+	{
+		std::cout << "Error occured! Error code = " << e.code() << ". Message: " << e.what();
+	}
+
+	
+	//--------------------------------------------------------------------------
+
+	
+
+	//system("pause");
+
+	return EXIT_SUCCESS;
+}
